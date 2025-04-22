141abb1b83b7ef56e58e18dd04e23076f79e25a6
anashm <anashammaoui07@gmail.com>
2025-04-22 11:42:58 +0100

    GS-5 fix member payment

--- resources/views/owner/members.blade.php
+++ resources/views/owner/members.blade.php
@@ -110,7 +110,7 @@
                         <svg class="h-4 w-4 mr-1 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                         </svg>
-                         {{ $member->created_at->addDays(30)->format('M j, Y') }}
+                         {{ $latestPayment ? $latestPayment->created_at->addDays(30)->format('M j, Y') : 'Not Payed Yet' }}
                     </div>
                 </td>
                 <td class="py-3 px-4">
